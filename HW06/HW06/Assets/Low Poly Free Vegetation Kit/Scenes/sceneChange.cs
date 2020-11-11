7144db450ba0ffaea161d8fcbd703d8cb837fe73
lyingbird <13261561717@163.com>
2020-11-12 00:05:30 +0800

    added pdf

---
+++ HW06/HW06/Assets/Low Poly Free Vegetation Kit/Scenes/sceneChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void gotoOriginalScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void gotoChangedScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
